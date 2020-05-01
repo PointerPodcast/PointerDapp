91c8d9e593ee57850a019edb72c63edef8cdb379
Alessandro Berti <alessandro.berti94@hotmail.it>
2020-05-01 00:59:18 +0000

    Refactoring

--- frontend/ethereumchat/src/Scene/Home/Home.js
+++ frontend/ethereumchat/src/Scene/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect} from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import { AppBar, Typography, List } from '@material-ui/core';
@@ -33,7 +33,7 @@ const useStyles = makeStyles((theme) => ({
         paddingTop: theme.spacing(7),
     }
 }));
-const elements = ['one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'one', 'two', 'three', "hello world", 'one', 'two', 'three', "hello world", 'one', 'two', 'three', "hello world", 'one', 'two', 'three', "hello world", 'one', 'two', 'three', "hello world"];
+
 
 const Home = () => {
     const classes = useStyles();
@@ -41,130 +41,186 @@ const Home = () => {
     const { height, width } = useWindowDimensions();
     const [open, setOpen] = React.useState(false);
     const [groupName, setGroupName] = useState('');
-    const web3 = useRef();
-    const accounts = useRef();
-    const contratto = useRef();
-    const [address, setAddress] = useState('');
     const [render, setRender] = React.useState(false);
     const [groupNames, setGroupNames] = useState([]);
-    const [GroupAddresses, setGroupAddresses] = useState([]);
     const [selectedGroup, setSelectedGroup] = useState('')
     const [username, setUsername] = useState('')
 
     const [messages, setMessages] = useState([])
-    const [openLogin, setOpenLogin] = React.useState(true);
+    const [lastGroupBlock, setLastGroupBlock] = useState({})
+    const [openLogin, setOpenLogin] = React.useState(false);
     const [loginUsername, setLoginUsername] = useState('');
 
+    const web3 = new Web3(Web3.givenProvider);
+    const contract = new web3.eth.Contract(TIME_MACHINE_ABI, TIME_MACHINE_ADDRESS)
 
-    useEffect(() => {
-        async function anyNameFunction() {
-            console.log("refresh")
-            //web3.current = new Web3('HTTP://127.0.0.1:8545')
-            web3.current = new Web3(Web3.givenProvider)
-            accounts.current = await web3.current.eth.getAccounts()
-            setAddress(accounts.current[0])
-            contratto.current = new web3.current.eth.Contract(
-                TIME_MACHINE_ABI,
-                TIME_MACHINE_ADDRESS
-            )
 
-            contratto.current.methods
-                .getGroups()
-                .call({ from: address })
-                .then((result) => {
-                    Promise.all(
-                        result.map(async (groupAddress) => {
-                            const groupContract = new web3.current.eth.Contract(
-                                GROUPS_ABI,
-                                groupAddress
-                            );
-                            setGroupAddresses([])
-                            var name = await groupContract.methods
-                                .getGroupName()
-                                .call({ from: address })
-                                .then((result) => {
-                                    setGroupAddresses(GroupAddresses => GroupAddresses.concat(groupAddress));
-                                    return web3.current.utils.toUtf8(result)
-                                });
 
-                            return { groupName: name, addressG: groupAddress }
-                        })
-                    ).then(function (results) {
-                        console.log(results.length)
-                        setGroupNames(results);
-                    });
-                });
+    function getUsernameMethod(){
+        web3.eth.getAccounts().then( (accounts) =>
+            contract.methods
+            .getUsername()
+            .call({ from: accounts[0] })
+            .then((result) => {
+                var utf8Username = web3.utils.toUtf8(result)
+                if (utf8Username === '')
+                    setOpenLogin(true)
+                else {
+                    setUsername(web3.utils.toUtf8(result))
+                    setOpenLogin(false)
+                }
+            })
+        ); 
+    }
 
-            contratto.current.methods
-                .getUsername()
-                .call({ from: accounts.current[0] })
-                .then((result) => {
-                    try {
-                        if (result == "0x0000000000000000000000000000000000000000000000000000000000000000")
-                            setOpenLogin(true)
-                        else {
-                            console.log(result)
-                            setUsername(web3.current.utils.toUtf8(result))
-                            console.log(web3.current.utils.toUtf8(result))
-                            setOpenLogin(false)
-                        }
-                    } catch (error) {
-                        console.log(error)
+    function setUsernameMethod(){
+        web3.eth.getAccounts().then( (accounts) =>
+            contract.methods
+                .setUsername(web3.utils.fromAscii(loginUsername))
+                .send({
+                    from: accounts[0]
+                })
+                .on("confirmation", (confirmationNumber, receipt) => {
+                    setOpenLogin(false);
+                    setUsername(loginUsername);
+                })
+                .on("error", (error) => {
+                    if (error.message.includes('Address already registered')) {
+                        setOpenLogin(false);
                     }
+                })
+        ) 
+    }
+
+    function getGroupsMethod(){
+        web3.eth.getAccounts().then( (accounts) =>
+            contract.methods
+            .getGroups()
+            .call({ from: accounts[0] })
+            .then((result) => {
+                Promise.all(
+                    result.map(async (groupAddress) => {
+                        const groupContract = new web3.eth.Contract(
+                            GROUPS_ABI,
+                            groupAddress
+                        );
+                        var name = await groupContract.methods
+                            .getGroupName()
+                            .call({ from: accounts[0] })
+                            .then((result) => { 
+                                lastGroupBlock[groupAddress] = 0;
+                                setLastGroupBlock(lastGroupBlock);
+                                return web3.utils.toUtf8(result);
+                            });
+                        return { groupName: name, addressG: groupAddress }
+                    })
+                ).then(function (results) {
+                    setGroupNames(results);
                 });
+            })
+        );
+    }
+
+    const createGroup = async () => {
+        if (groupName === '') {
+            alert("This field cannot be empty");
+            return;
         }
-        anyNameFunction();
+        web3.eth.getAccounts().then( (accounts) =>
+            contract.methods
+                .createGroup(web3.utils.fromAscii(groupName))
+                .send({
+                    from: accounts[0],
+                })
+                .on("confirmation", (confirmationNumber, receipt) => {
+                    handleClose();
+                    getGroupsMethod();
+                })
+                .on("error", (error) => {
+                    console.log(error);
+                })
+        ); 
+    }
 
-    }, [render]);
 
-
-    useEffect(() => {
-        setMessages([])
-        console.log(groupNames)
-        for (var i = 0; i < groupNames.length; i++) {
-            const groupContract = new web3.current.eth.Contract(
+    function refreshGroupMessage(groupAddress){
+            const groupContract = new web3.eth.Contract(
                 GROUPS_ABI,
-                GroupAddresses[i]
+                groupAddress
             );
-
+            console.log(lastGroupBlock);
+            const lastSynchedBlock = lastGroupBlock[groupAddress];
             groupContract.events.Message({
-                fromBlock: 0
+                fromBlock: lastSynchedBlock,
             }, function (error, event) {
-                console.log(event)
+                web3.eth.getBlockNumber().then( (lastblock) => lastGroupBlock[groupAddress] = lastblock); //sync according to latest block read
                 setMessages(messages => messages.concat({
-                    name: web3.current.utils.toUtf8(event.returnValues.from),
-                    message: web3.current.utils.toUtf8(event.returnValues.message),
+                    name: web3.utils.toUtf8(event.returnValues.from),
+                    message: web3.utils.toUtf8(event.returnValues.message),
                     groupName: event.address,
                     hash: event.transactionHash
                 }));
 
             })
+    }
 
-        }
-    }, [groupNames]);
-
-
-
-    const createGroup = async () => {
-        if (groupName == '') {
-            alert("This field cannot be empty");
-        }
-        else {
-            contratto.current.methods
-                .createGroup(web3.current.utils.fromAscii(groupName))
+    function sendMessageMethod(message){
+        const groupContract = new web3.eth.Contract(
+            GROUPS_ABI,
+            selectedGroup
+        );
+        web3.eth.getAccounts().then( (accounts) => 
+            groupContract.methods
+                .sendEventMessage(web3.utils.fromAscii(message), false)
                 .send({
-                    from: address
-                    //gas: 1000000,
-                    //gasPrice: '1'
+                    from: accounts[0]
+        //          gas: 1000000,
+        //          gasPrice: '1'
                 })
                 .on("confirmation", (confirmationNumber, receipt) => {
-                    handleClose();
+                    console.log("Inviato")
+                    var boxDiv = document.getElementById("scrollBox");
+                    boxDiv.scrollTop = boxDiv.scrollHeight;
                 })
                 .on("error", (error) => {
                     console.log(error);
-                });
-        }
+                })
+        );
     }
+
+
+    useEffect(() => {
+        async function anyNameFunction() {
+            getUsernameMethod()
+            getGroupsMethod()
+        }
+
+        anyNameFunction();
+    }, [render]);
+
+
+    const sendMessage = () => {
+        var message = document.getElementById('multiline-static').value
+        if (message === '')
+            return
+        if (selectedGroup === '') {
+            alert("Please, select a group.")
+            return
+        }
+        sendMessageMethod(message)
+        document.getElementById('multiline-static').value = ""
+    }
+
+
+    const handleLoginClose = () => {
+        if (loginUsername === '') {
+            alert('Field Required');
+            return
+        }
+        setUsernameMethod()
+    };
+
+
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -175,75 +231,24 @@ const Home = () => {
         render ? setRender(false) : setRender(true);
     };
 
-    const sendMessage = () => {
-        var message = document.getElementById('multiline-static').value
-        if (selectedGroup == '') {
-            alert("Seleziona un gruppo in cui inviare il messaggio")
-        }
-        else if (message != '') {
-            const groupContract = new web3.current.eth.Contract(
-                GROUPS_ABI,
-                selectedGroup
-            );
-            groupContract.methods
-                .sendEventMessage(web3.current.utils.fromAscii(message), false)
-                .send({
-                    from: address
-                    //          gas: 1000000,
-                    //         gasPrice: '1'
-                })
-                .on("confirmation", (confirmationNumber, receipt) => {
-                    console.log("Inviato")
-                })
-                .on("error", (error) => {
-                    console.log(error);
-                });
-        }
-        document.getElementById('multiline-static').value = ""
-
-    }
-
-
-    const changeSelectedGroup = (address) => {
-        setSelectedGroup(address);
-        console.log(address)
-    };
-
-
-    const handleLoginClose = () => {
-        if (loginUsername == '') {
-            alert('Il campo username non può essere lasciato vuoto');
-        }
-        else {
-            contratto.current.methods
-                .setUsername(web3.current.utils.fromAscii(loginUsername))
-                .send({
-                    from: address,
-                })
-                .on("confirmation", (confirmationNumber, receipt) => {
-                    setOpenLogin(false);
-                })
-                .on("error", (error) => {
-                    if (error.message.includes('Address already registered')) {
-                        setOpenLogin(false);
-                    }
-                });
-        }
+    const changeSelectedGroup = (groupAddress) => {
+        setSelectedGroup(groupAddress);
+        refreshGroupMessage(groupAddress);
+        var boxDiv = document.getElementById("scrollBox");
+        boxDiv.scrollTop = boxDiv.scrollHeight;
     };
 
     var list = []
 
     const generateMessages =
-        messages.slice(0).reverse().map((value, _) => {
-            console.log(value.hash)
-            if (value.groupName == selectedGroup && list.indexOf(value.hash) === -1) {
-                console.log(value)
+        messages.slice(0).map((value, _) => {
+            if (value.groupName === selectedGroup && list.indexOf(value.hash) === -1) {
                 list.push(value.hash)
+                var boxDiv = document.getElementById("scrollBox");
+                boxDiv.scrollTop = boxDiv.scrollHeight;
                 return (<Message sender={value.name} body={value.message}></Message>)
-
             }
         })
-
 
 
     return (
@@ -252,7 +257,7 @@ const Home = () => {
                 <Toolbar variant="dense">
 
                     <Typography variant="h6" color="inherit" className={classes.title}>
-                        EthereumChat
+                        <b>PointerDapp</b>
                         </Typography>
                     <Button color="inherit">{username}</Button>
                 </Toolbar>
@@ -264,7 +269,7 @@ const Home = () => {
                 <DialogContent>
                     <DialogContentText>
                         Insert your username to use the chat!
-          </DialogContentText>
+                    </DialogContentText>
                     <TextField
                         autoFocus
                         id="name"
@@ -278,7 +283,7 @@ const Home = () => {
 
                     <Button onClick={handleLoginClose} color="primary">
                         Login
-          </Button>
+                    </Button>
                 </DialogActions>
             </Dialog>
 
@@ -323,9 +328,8 @@ const Home = () => {
 
                 <Grid item xs={12} sm={7} md={9} xl={9} spacing={3} >
                     <Box >
-                        <Box height={height - 156} style={{ overflow: 'auto' }}>
+                        <Box id="scrollBox" height={height - 156} style={{ overflow: 'auto' }}>
                             {generateMessages}
-
                         </Box>
                         <Box>
                             <Grid container >
--- frontend/ethereumchat/src/index.js
+++ frontend/ethereumchat/src/index.js
@@ -4,13 +4,11 @@ import { Route, BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 import Home from "./Scene/Home/Home";
-import Login from "./Scene/Login/Login";
 
 const routing = (
   <Router>
     <div>
       <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
     </div>
   </Router>
 );
