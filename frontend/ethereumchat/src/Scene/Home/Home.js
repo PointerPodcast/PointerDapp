d8627a18db7f9c149fe5aadb75f3f057ad8c2188
lucacorbucci <tankado@live.it>
2020-05-01 09:46:54 +0200

    Added a check to understand if web3 is connected or not

--- frontend/ethereumchat/src/Scene/Home/Home.js
+++ frontend/ethereumchat/src/Scene/Home/Home.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import { AppBar, Typography, List } from '@material-ui/core';
+import { AppBar, Typography } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
-import Paper from '@material-ui/core/Paper';
 import Box from "@material-ui/core/Box";
 import Message from "./Components/Message"
 import useWindowDimensions from '../../Hooks/useWindowDimension';
@@ -45,36 +44,35 @@ const Home = () => {
     const [groupNames, setGroupNames] = useState([]);
     const [selectedGroup, setSelectedGroup] = useState('')
     const [username, setUsername] = useState('')
-
     const [messages, setMessages] = useState([])
     const [lastGroupBlock, setLastGroupBlock] = useState({})
     const [openLogin, setOpenLogin] = React.useState(false);
     const [loginUsername, setLoginUsername] = useState('');
-
+    var list = []
     const web3 = new Web3(Web3.givenProvider);
     const contract = new web3.eth.Contract(TIME_MACHINE_ABI, TIME_MACHINE_ADDRESS)
+    const [isMetamaskInstalled, setMetamask] = useState(false);
 
 
-
-    function getUsernameMethod(){
-        web3.eth.getAccounts().then( (accounts) =>
+    function getUsernameMethod() {
+        web3.eth.getAccounts().then((accounts) =>
             contract.methods
-            .getUsername()
-            .call({ from: accounts[0] })
-            .then((result) => {
-                var utf8Username = web3.utils.toUtf8(result)
-                if (utf8Username === '')
-                    setOpenLogin(true)
-                else {
-                    setUsername(web3.utils.toUtf8(result))
-                    setOpenLogin(false)
-                }
-            })
-        ); 
+                .getUsername()
+                .call({ from: accounts[0] })
+                .then((result) => {
+                    var utf8Username = web3.utils.toUtf8(result)
+                    if (utf8Username === '')
+                        setOpenLogin(true)
+                    else {
+                        setUsername(web3.utils.toUtf8(result))
+                        setOpenLogin(false)
+                    }
+                })
+        );
     }
 
-    function setUsernameMethod(){
-        web3.eth.getAccounts().then( (accounts) =>
+    function setUsernameMethod() {
+        web3.eth.getAccounts().then((accounts) =>
             contract.methods
                 .setUsername(web3.utils.fromAscii(loginUsername))
                 .send({
@@ -89,35 +87,35 @@ const Home = () => {
                         setOpenLogin(false);
                     }
                 })
-        ) 
+        )
     }
 
-    function getGroupsMethod(){
-        web3.eth.getAccounts().then( (accounts) =>
+    function getGroupsMethod() {
+        web3.eth.getAccounts().then((accounts) =>
             contract.methods
-            .getGroups()
-            .call({ from: accounts[0] })
-            .then((result) => {
-                Promise.all(
-                    result.map(async (groupAddress) => {
-                        const groupContract = new web3.eth.Contract(
-                            GROUPS_ABI,
-                            groupAddress
-                        );
-                        var name = await groupContract.methods
-                            .getGroupName()
-                            .call({ from: accounts[0] })
-                            .then((result) => { 
-                                lastGroupBlock[groupAddress] = 0;
-                                setLastGroupBlock(lastGroupBlock);
-                                return web3.utils.toUtf8(result);
-                            });
-                        return { groupName: name, addressG: groupAddress }
-                    })
-                ).then(function (results) {
-                    setGroupNames(results);
-                });
-            })
+                .getGroups()
+                .call({ from: accounts[0] })
+                .then((result) => {
+                    Promise.all(
+                        result.map(async (groupAddress) => {
+                            const groupContract = new web3.eth.Contract(
+                                GROUPS_ABI,
+                                groupAddress
+                            );
+                            var name = await groupContract.methods
+                                .getGroupName()
+                                .call({ from: accounts[0] })
+                                .then((result) => {
+                                    lastGroupBlock[groupAddress] = 0;
+                                    setLastGroupBlock(lastGroupBlock);
+                                    return web3.utils.toUtf8(result);
+                                });
+                            return { groupName: name, addressG: groupAddress }
+                        })
+                    ).then(function (results) {
+                        setGroupNames(results);
+                    });
+                })
         );
     }
 
@@ -126,7 +124,7 @@ const Home = () => {
             alert("This field cannot be empty");
             return;
         }
-        web3.eth.getAccounts().then( (accounts) =>
+        web3.eth.getAccounts().then((accounts) =>
             contract.methods
                 .createGroup(web3.utils.fromAscii(groupName))
                 .send({
@@ -139,43 +137,43 @@ const Home = () => {
                 .on("error", (error) => {
                     console.log(error);
                 })
-        ); 
+        );
     }
 
 
-    function refreshGroupMessage(groupAddress){
-            const groupContract = new web3.eth.Contract(
-                GROUPS_ABI,
-                groupAddress
-            );
-            console.log(lastGroupBlock);
-            const lastSynchedBlock = lastGroupBlock[groupAddress];
-            groupContract.events.Message({
-                fromBlock: lastSynchedBlock,
-            }, function (error, event) {
-                web3.eth.getBlockNumber().then( (lastblock) => lastGroupBlock[groupAddress] = lastblock); //sync according to latest block read
-                setMessages(messages => messages.concat({
-                    name: web3.utils.toUtf8(event.returnValues.from),
-                    message: web3.utils.toUtf8(event.returnValues.message),
-                    groupName: event.address,
-                    hash: event.transactionHash
-                }));
+    function refreshGroupMessage(groupAddress) {
+        const groupContract = new web3.eth.Contract(
+            GROUPS_ABI,
+            groupAddress
+        );
+        console.log(lastGroupBlock);
+        const lastSynchedBlock = lastGroupBlock[groupAddress];
+        groupContract.events.Message({
+            fromBlock: lastSynchedBlock,
+        }, function (error, event) {
+            web3.eth.getBlockNumber().then((lastblock) => lastGroupBlock[groupAddress] = lastblock); //sync according to latest block read
+            setMessages(messages => messages.concat({
+                name: web3.utils.toUtf8(event.returnValues.from),
+                message: web3.utils.toUtf8(event.returnValues.message),
+                groupName: event.address,
+                hash: event.transactionHash
+            }));
 
-            })
+        })
     }
 
-    function sendMessageMethod(message){
+    function sendMessageMethod(message) {
         const groupContract = new web3.eth.Contract(
             GROUPS_ABI,
             selectedGroup
         );
-        web3.eth.getAccounts().then( (accounts) => 
+        web3.eth.getAccounts().then((accounts) =>
             groupContract.methods
                 .sendEventMessage(web3.utils.fromAscii(message), false)
                 .send({
                     from: accounts[0]
-        //          gas: 1000000,
-        //          gasPrice: '1'
+                    //          gas: 1000000,
+                    //          gasPrice: '1'
                 })
                 .on("confirmation", (confirmationNumber, receipt) => {
                     console.log("Inviato")
@@ -190,13 +188,20 @@ const Home = () => {
 
 
     useEffect(() => {
-        async function anyNameFunction() {
+        async function setup() {
+
             getUsernameMethod()
             getGroupsMethod()
+            setMetamask(true);
         }
 
-        anyNameFunction();
-    }, [render]);
+        web3.eth.net.isListening().then((s) => {
+            setup();
+        }).catch((e) => {
+
+        })
+
+    }, [getGroupsMethod, getUsernameMethod, render]);
 
 
     const sendMessage = () => {
@@ -238,7 +243,6 @@ const Home = () => {
         boxDiv.scrollTop = boxDiv.scrollHeight;
     };
 
-    var list = []
 
     const generateMessages =
         messages.slice(0).map((value, _) => {
@@ -251,6 +255,7 @@ const Home = () => {
         })
 
 
+
     return (
         <div className={classes.root}>
             <AppBar>
@@ -258,105 +263,122 @@ const Home = () => {
 
                     <Typography variant="h6" color="inherit" className={classes.title}>
                         <b>PointerDapp</b>
-                        </Typography>
+                    </Typography>
                     <Button color="inherit">{username}</Button>
                 </Toolbar>
             </AppBar>
-
-
-            <Dialog open={openLogin} aria-labelledby="form-dialog-title">
-                <DialogTitle id="form-dialog-title">Login</DialogTitle>
-                <DialogContent>
-                    <DialogContentText>
-                        Insert your username to use the chat!
+            {isMetamaskInstalled ? (
+                <div>
+                    <Dialog open={openLogin} aria-labelledby="form-dialog-title">
+                        <DialogTitle id="form-dialog-title">Login</DialogTitle>
+                        <DialogContent>
+                            <DialogContentText>
+                                Insert your username to use the chat!
                     </DialogContentText>
-                    <TextField
-                        autoFocus
-                        id="name"
-                        label="Username"
-                        type="string"
-                        fullWidth
-                        onChange={e => setLoginUsername(e.target.value)}
-                    />
-                </DialogContent>
-                <DialogActions>
+                            <TextField
+                                autoFocus
+                                id="name"
+                                label="Username"
+                                type="string"
+                                fullWidth
+                                onChange={e => setLoginUsername(e.target.value)}
+                            />
+                        </DialogContent>
+                        <DialogActions>
 
-                    <Button onClick={handleLoginClose} color="primary">
-                        Login
+                            <Button onClick={handleLoginClose} color="primary">
+                                Login
                     </Button>
-                </DialogActions>
-            </Dialog>
+                        </DialogActions>
+                    </Dialog>
 
 
 
-            <Grid container spacing={1} className={classes.container}>
-                <Grid item xs={12} sm={5} md={3} xl={3}>
-                    <Container>
-                        <Button variant="contained" color="primary" onClick={handleClickOpen}>
-                            Add Group
+                    <Grid container spacing={1} className={classes.container}>
+                        <Grid item xs={12} sm={5} md={3} xl={3}>
+                            <Container>
+                                <Button variant="contained" color="primary" onClick={handleClickOpen}>
+                                    Add Group
                         </Button>
-                        <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-                            <DialogTitle id="form-dialog-title">Create a new Group</DialogTitle>
+                                <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                                    <DialogTitle id="form-dialog-title">Create a new Group</DialogTitle>
+                                    <DialogContent>
+                                        <DialogContentText>
+                                            Insert the group name:
+                                </DialogContentText>
+                                        <TextField
+                                            autoFocus
+                                            id="name"
+                                            label="Group Name"
+                                            type="string"
+                                            fullWidth
+                                            onChange={e => setGroupName(e.target.value)}
+                                        />
+                                    </DialogContent>
+                                    <DialogActions>
+                                        <Button onClick={handleClose} color="primary">
+                                            Cancel
+                                </Button>
+                                        <Button onClick={createGroup} color="primary">
+                                            Create Group
+                                 </Button>
+                                    </DialogActions>
+                                </Dialog>
+
+                                <GroupList groupNames={groupNames} changeGroup={changeSelectedGroup} ></GroupList>
+                            </Container>
+
+                        </Grid>
+
+
+                        <Grid item xs={12} sm={7} md={9} xl={9} spacing={3} >
+                            <Box >
+                                <Box id="scrollBox" height={height - 156} style={{ overflow: 'auto' }}>
+                                    {generateMessages}
+                                </Box>
+                                <Box>
+                                    <Grid container >
+                                        <Grid item xs={10}>
+                                            <TextField
+                                                id="multiline-static"
+                                                fullWidth
+                                                multiline
+                                                rows="2"
+                                                InputLabelProps={{
+                                                    shrink: true
+                                                }}
+                                                placeholder="Start writing your message"
+                                                className={classes.textField}
+
+                                            />
+                                        </Grid>
+                                        <Grid item xs={2}>
+                                            <Button color="primary" onClick={sendMessage}>
+                                                Send
+                                    </Button>
+                                        </Grid>
+                                    </Grid>
+                                </Box>
+                            </Box>
+                        </Grid>
+                    </Grid >
+                </div>
+            ) : (
+                    <div>
+                        <Dialog open={true} aria-labelledby="form-dialog-title">
+                            <DialogTitle id="form-dialog-title">Login</DialogTitle>
                             <DialogContent>
                                 <DialogContentText>
-                                    Insert the group name:
-                                </DialogContentText>
-                                <TextField
-                                    autoFocus
-                                    id="name"
-                                    label="Group Name"
-                                    type="string"
-                                    fullWidth
-                                    onChange={e => setGroupName(e.target.value)}
-                                />
+                                    Install Metamask to use PointerDapp
+                            </DialogContentText>
+
                             </DialogContent>
-                            <DialogActions>
-                                <Button onClick={handleClose} color="primary">
-                                    Cancel
-                                </Button>
-                                <Button onClick={createGroup} color="primary">
-                                    Create Group
-                                 </Button>
-                            </DialogActions>
+
                         </Dialog>
-
-                        <GroupList groupNames={groupNames} changeGroup={changeSelectedGroup} ></GroupList>
-                    </Container>
-
-                </Grid>
+                    </div>
+                )}
 
 
-                <Grid item xs={12} sm={7} md={9} xl={9} spacing={3} >
-                    <Box >
-                        <Box id="scrollBox" height={height - 156} style={{ overflow: 'auto' }}>
-                            {generateMessages}
-                        </Box>
-                        <Box>
-                            <Grid container >
-                                <Grid item xs={10}>
-                                    <TextField
-                                        id="multiline-static"
-                                        fullWidth
-                                        multiline
-                                        rows="2"
-                                        InputLabelProps={{
-                                            shrink: true
-                                        }}
-                                        placeholder="Start writing your message"
-                                        className={classes.textField}
-
-                                    />
-                                </Grid>
-                                <Grid item xs={2}>
-                                    <Button color="primary" onClick={sendMessage}>
-                                        Send
-                                    </Button>
-                                </Grid>
-                            </Grid>
-                        </Box>
-                    </Box>
-                </Grid>
-            </Grid >
 
 
 
